358c249142f634e7e06560a347b1ee54e42b2cb6
pritibhondwe <bhondwep88@gmail.com>
2025-12-18 15:59:32 +0530

    Update Projects.js
    delete the project.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,1 @@
-function Projects() {
-  return (
-    <section>
-      <h2>Projects</h2>
-      <ul>
-        <li>Portfolio Website – ReactJS</li>
-        <li>Library Management System – Java</li>
-        <li>Quiz Application – Java</li>
-      </ul>
-    </section>
-  );
-}
 
-export default Projects;
